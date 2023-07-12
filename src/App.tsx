9a624553b90acaade24a89ba383cc7036970d56e
ivand150 <ivandobry@hotmail.es>
2023-07-12 11:49:02 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+
+      </div>
+      <h1>Harina refinada</h1>
+      <div className="card">
+
+        <p>
+          coming soon...
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
